c8914fc773b8890ac983643790026e81c1d9221d
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-09-18 13:25:55 -0300

    feat: added food and calorie data on screen

--- app/src/main/java/com/myexample/alimentos/adapter/FoodAdapter.java
+++ app/src/main/java/com/myexample/alimentos/adapter/FoodAdapter.java
@@ -1,10 +1,14 @@
 package com.myexample.alimentos.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.myexample.alimentos.R;
 import com.myexample.alimentos.entity.FoodEntity;
 import com.myexample.alimentos.viewholder.FoodViewHolder;
 
@@ -20,12 +24,18 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodViewHolder> {
     @NonNull
     @Override
     public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.item_food, parent, false);
+
+        return new FoodViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
-
+        FoodEntity food = this.mList.get(position);
+        holder.bind(food);
     }
 
     @Override
--- app/src/main/java/com/myexample/alimentos/viewholder/FoodViewHolder.java
+++ app/src/main/java/com/myexample/alimentos/viewholder/FoodViewHolder.java
@@ -1,12 +1,28 @@
 package com.myexample.alimentos.viewholder;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.myexample.alimentos.R;
+import com.myexample.alimentos.entity.FoodEntity;
+
 public class FoodViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView mTextName;
+    private TextView mTextCalorie;
+
     public FoodViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        this.mTextName = itemView.findViewById(R.id.text_name);
+        this.mTextCalorie = itemView.findViewById(R.id.text_calories);
     }
+
+        public void bind(FoodEntity food) {
+            this.mTextName.setText(food.getName());
+            this.mTextCalorie.setText(String.valueOf(food.getCalories()));
+        }
 }
